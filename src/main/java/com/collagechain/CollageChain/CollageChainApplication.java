04edbb0c3424b43f73edf026fd932765b1ca0e62
AmitTiwar <63752846+AmitTiwar@users.noreply.github.com>
2020-04-16 02:57:17 +0530

    Add files via upload

---
+++ src/main/java/com/collagechain/CollageChain/CollageChainApplication.java
@@ -0,0 +1,13 @@
+package com.collagechain.CollageChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollageChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollageChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collagechain/CollageChain/CollageChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collagechain.CollageChain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollageChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
